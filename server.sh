e2cf41c7c2aa45a3611028f915b308dbddd2ab88
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-08 23:44:42 +0100

    Delete server.sh

--- server.sh
+++
@@ -1,8 +0,0 @@
-printf "\033c\033[43;30m\nremote shell....\n"
-rm -f /tmp/f
-mkfifo /tmp/f
-npx localtunnel --port 8080 &
-while true ;
-do
-    cat /tmp/f | /bin/sh -i 2>&1 | nc -l 8080 > /tmp/f
-done
