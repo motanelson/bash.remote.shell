b1243eae101fa9f51f4ba7a89efce0f1f9b245c6
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-08 21:42:16 +0100

    Add files via upload

--- inet.sh
+++ inet.sh
@@ -1,4 +1,9 @@
 printf "\033c\033[43;30m\n"
 python3 -m http.server --cgi 8080 &
-npx localtunnel --port 8080
+npx localtunnel --port 8080 > out.txt &
+sleep 20
+a=$(cat out.txt | sed -E 's/.*(https?:\/\/[^ ]+).*/\1/')
+echo ......
+echo $a
+bash keep.sh $a >/dev/null
 
